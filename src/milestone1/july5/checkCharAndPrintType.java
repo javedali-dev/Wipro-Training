f9d7adf219cd44695038be52e2aa4da5a63e309b
Javedali0563 <javedali0563@gmail.com>
2021-07-06 15:11:47 +0530

    :cat2: added checkCharAndPrintType

---
+++ src/milestone1/july5/checkCharAndPrintType.java
@@ -0,0 +1,21 @@
+/* Initialize a character variable in a program and
+if the value is alphabet then print "Alphabet" if it’s a number then print "Digit"
+and for other characters print "Special Character"
+ */
+
+package milestone1.july5;
+
+import java.util.Scanner;
+
+public class checkCharAndPrintType {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char ch = scanner.next().charAt(0);
+        if(Character.isDigit(ch))
+            System.out.println("Digit");
+        else if(Character.isLetter(ch))
+            System.out.println("Alphabet");
+        else
+            System.out.println("Special Character");
+    }
+}
